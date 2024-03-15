3c8afc3a0e210659721335f745dcac8c96e604de
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-03-15 09:34:38 +0200

    printZ added description

--- Varia/printZ.py
+++ Varia/printZ.py
@@ -1,2 +1,6 @@
+"""Write a program that takes a string, and displays the first 'z'
+character it encounters in it, followed by a newline. If there are no
+'z' characters in the string, the program writes 'z' followed
+by a newline."""
 user_input = input()
 print("z")
